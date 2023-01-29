138d2a0d226db4da01f4b2c6dfb525d1f8c2e814
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-29 18:44:33 +0100

    jsnack 3 attempt

---
+++ jsnack3/js/main.js
@@ -0,0 +1,17 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+// ARRAY
+
+const numbers = [4, 67, 78, 43, 5, 98, 7];
+
+console.log(numbers);
+
+// CALC FORMULA
+
+for (let i = 0; i <= numbers.length; i++) {
+
+    // if (numbers[i] = i + 2) {
+    //     console.log(numbers[i]);
+    // }
+
+}
